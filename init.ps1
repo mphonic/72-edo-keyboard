c30b42939bedb51dc49fe9f41bc4b86ab931027d
Bhob Rainey <lloydlyson@gmail.com>
2019-07-14 00:22:10 -0400

    Add internal option. Split note and bend msgs

--- init.ps1
+++ init.ps1
@@ -1,7 +1,8 @@
-Function Init {
+param([String]$destination='reaper')
+Function Init($destination = 'reaper') {
 forever start .\index.js;
 Start-Process "chrome.exe" .\web\index.html;
-sclang.exe .\capture-osc.scd;
+sclang.exe ".\capture-osc-$($destination).scd";
 }
 
-Init
+Init($destination)
